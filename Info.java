6156c683947620a2ea7b0f6a4b92e57fb8bfbb4e
FaseehUrRehman662 <faseehurrehman662@gmail.com>
2025-03-07 11:43:02 -0800

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,15 @@
+public class Info{
+      int age;
+      String name;
+
+   public Info(int age, String name){
+      this.age = age;
+      this.name = name;
+   } 
+
+   public void display(){
+       System.out.println("Name: " + name);
+       System.out.println("Age: " + age);
+   }
+
+} 
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+  public static void main(String[]args){
+
+      Info obj = new Info(20, "fasi");
+
+      obj.display();
+
+  }
+}
